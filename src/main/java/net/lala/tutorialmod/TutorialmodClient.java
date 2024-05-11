c216ef1e0a00af637e0a0a281c06215765ce83d0
lalaldka <168130690+lalaldka@users.noreply.github.com>
2024-05-10 21:57:26 -0400

    something?

---
+++ src/main/java/net/lala/tutorialmod/TutorialmodClient.java
@@ -0,0 +1,10 @@
+package net.lala.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
